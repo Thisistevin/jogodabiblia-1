1d4ae26f332690009f9071285ca8544777c21845
Estevão Alves Moreira <159822821+Thisistevin@users.noreply.github.com>
2024-04-23 18:47:22 -0300

    Font Family do button

--- src/Components/Button/styles.js
+++ src/Components/Button/styles.js
@@ -5,5 +5,8 @@ export const Container = styled.div`
 `;
 
 export const ButtonComponent = styled.button`
-    background-color: #3F5C70;
+    background-color: #547B96;
+    padding: 11px 27px;
+    border: solid 1px #3F5C70;
+    font-family: "Lato", sans-serif;
 `;
